0d5cf9c7e3f7a0f9b20f47b1ee189d4b0f990fff
Mvwivs <mvwivs@gmail.com>
2020-12-28 02:02:36 +0300

    cassowary: add paddings

--- src/diagram/nested.tsx
+++ src/diagram/nested.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Graph, Addon, Markup, Node, CellView, Cell } from "@antv/x6";
 import * as kiwi from "kiwi.js";
-// import '../index.less'
-import { test_data } from "./example-data";
+
 import { get_data } from "./get_data";
 import { ReactShape } from "@antv/x6-react-shape";
 import "@antv/x6-react-shape";
@@ -10,10 +9,6 @@ import "@antv/x6-react-shape";
 import { NodeShape } from "./NodeShape";
 import { Compartment } from "./Compartment";
 import { NodeField } from "./NodeField";
-import { nodeCenter } from "@antv/x6/lib/registry/node-anchor/main";
-
-import { cpuUsage } from "process";
-import { Constraint } from "kiwi.js";
 
 const { Stencil } = Addon;
 
@@ -257,39 +252,36 @@ export class G extends React.Component<MyProps, {}> {
 				left: new kiwi.Variable(),
 				width: new kiwi.Variable(),
 				height: new kiwi.Variable(),
+				padding: null,
 				constraints: [],
 			};
 			const n = data[node.id];
+
 			n.constraints = [
-				new kiwi.Constraint(
-					n.width,
-					kiwi.Operator.Ge,
-					100,
-					kiwi.Strength.required
-				),
-				new kiwi.Constraint(
-					n.height,
-					kiwi.Operator.Ge,
-					50,
-					kiwi.Strength.required
-				),
+				new kiwi.Constraint(n.width, kiwi.Operator.Ge, 200, kiwi.Strength.required),
+				new kiwi.Constraint(n.height, kiwi.Operator.Ge, 40, kiwi.Strength.required)
 			];
+
 			if (node.shape === "field") {
-				solver.addEditVariable(data[node.id].top, kiwi.Strength.weak);
-				solver.addEditVariable(data[node.id].left, kiwi.Strength.weak);
-				solver.addEditVariable(data[node.id].width, kiwi.Strength.weak);
-				solver.addEditVariable(data[node.id].height, kiwi.Strength.strong);
+				solver.addEditVariable(n.top, kiwi.Strength.weak);
+				solver.addEditVariable(n.left, kiwi.Strength.weak);
+				solver.addEditVariable(n.width, kiwi.Strength.weak);
+				solver.addEditVariable(n.height, kiwi.Strength.strong);
+				n.padding = { top: 0, bottom: 0, left: 0, right: 0 };
 			} else if (node.shape === "compartment") {
 				solver.addEditVariable(n.top, kiwi.Strength.medium);
 				solver.addEditVariable(n.left, kiwi.Strength.medium);
 				solver.addEditVariable(n.width, kiwi.Strength.weak);
 				solver.addEditVariable(n.height, kiwi.Strength.weak);
+				n.padding = { top: 30, bottom: 10, left: 5, right: 5 };
 			} else {
 				solver.addEditVariable(n.top, kiwi.Strength.strong);
 				solver.addEditVariable(n.left, kiwi.Strength.strong);
 				solver.addEditVariable(n.width, kiwi.Strength.strong);
 				solver.addEditVariable(n.height, kiwi.Strength.weak);
+				n.padding = { top: 30, bottom: 10, left: 5, right: 5 };
 			}
+
 			solver.suggestValue(n.left, node.position().x);
 			solver.suggestValue(n.top, node.position().y);
 			for (const constraint of n.constraints) {
@@ -315,6 +307,31 @@ export class G extends React.Component<MyProps, {}> {
 			descendants_ids.add(desc.id);
 		}
 		return descendants_ids;
+	}
+
+	update_parent(parent) {
+		const { solver, data } = this.size_tree;
+
+		if (parent.children.constraint) {
+			solver.removeConstraint(parent.children.constraint);
+		}
+		if (Object.keys(parent.children.data).length !== 0) {
+			let parent_size = new kiwi.Expression(parent.padding.top);
+			let offset = new kiwi.Expression(parent.top, parent.padding.top);
+			for (const child_id in parent.children.data) {
+				const child = data[child_id];
+				if (parent.children.data[child_id]) {
+					solver.removeConstraint(parent.children.data[child_id]);
+				}
+				const child_offset = new kiwi.Constraint(child.top, kiwi.Operator.Eq, offset, kiwi.Strength.required);
+				parent.children.data[child_id] = child_offset;
+				solver.addConstraint(child_offset);
+				offset = new kiwi.Expression(child.top, child.height);
+				parent_size = parent_size.plus(child.height);
+			}
+			parent.children.constraint = new kiwi.Constraint(parent_size.plus(parent.padding.bottom), kiwi.Operator.Eq, parent.height, kiwi.Strength.required);
+			solver.addConstraint(parent.children.constraint);
+		}
 	}
 
 	size_calc = (e, type) => {
@@ -350,51 +367,17 @@ export class G extends React.Component<MyProps, {}> {
 				parent.children.data[updated] = null;
 
 				u.parent.constraints = [
-					new kiwi.Constraint(
-						u.width,
-						kiwi.Operator.Eq,
-						parent.width,
-						kiwi.Strength.required
-					),
-					new kiwi.Constraint(
-						u.left,
-						kiwi.Operator.Eq,
-						parent.left,
-						kiwi.Strength.required
-					),
+					new kiwi.Constraint(u.width, kiwi.Operator.Eq,
+						new kiwi.Expression(parent.width, -parent.padding.right, -parent.padding.left),
+						kiwi.Strength.required),
+					new kiwi.Constraint(u.left, kiwi.Operator.Eq,
+						new kiwi.Expression(parent.left, parent.padding.left), kiwi.Strength.required),
 				];
 				for (const constraint of u.parent.constraints) {
 					solver.addConstraint(constraint);
 				}
 
-				let parent_size = new kiwi.Expression();
-				let offset = new kiwi.Expression(parent.top, 20);
-				for (const child_id in parent.children.data) {
-					const child = data[child_id];
-					if (parent.children.data[child_id]) {
-						solver.removeConstraint(parent.children.data[child_id]);
-					}
-					const child_offset = new kiwi.Constraint(
-						child.top,
-						kiwi.Operator.Eq,
-						offset,
-						kiwi.Strength.required
-					);
-					parent.children.data[child_id] = child_offset;
-					solver.addConstraint(child_offset);
-					offset = new kiwi.Expression(child.top, child.height);
-					parent_size = parent_size.plus(child.height);
-				}
-				if (parent.children.constraint) {
-					solver.removeConstraint(parent.children.constraint);
-				}
-				parent.children.constraint = new kiwi.Constraint(
-					parent_size,
-					kiwi.Operator.Eq,
-					parent.height,
-					kiwi.Strength.required
-				);
-				solver.addConstraint(parent.children.constraint);
+				this.update_parent(parent);
 
 				changed = new Set([...changed, ...this.propogade_update(updated)]);
 			} else {
@@ -405,36 +388,8 @@ export class G extends React.Component<MyProps, {}> {
 				const parent = data[node.id];
 				solver.removeConstraint(parent.children.data[updated]);
 				delete parent.children.data[updated];
-				if (parent.children.constraint) {
-					solver.removeConstraint(parent.children.constraint);
-				}
-				if (Object.keys(parent.children.data).length !== 0) {
-					let parent_size = new kiwi.Expression();
-					let offset = new kiwi.Expression(parent.top, 20);
-					for (const child_id in parent.children.data) {
-						const child = data[child_id];
-						if (parent.children.data[child_id]) {
-							solver.removeConstraint(parent.children.data[child_id]);
-						}
-						const child_offset = new kiwi.Constraint(
-							child.top,
-							kiwi.Operator.Eq,
-							offset,
-							kiwi.Strength.required
-						);
-						parent.children.data[child_id] = child_offset;
-						solver.addConstraint(child_offset);
-						offset = new kiwi.Expression(child.top, child.height);
-						parent_size = parent_size.plus(child.height);
-					}
-					parent.children.constraint = new kiwi.Constraint(
-						parent_size,
-						kiwi.Operator.Eq,
-						parent.height,
-						kiwi.Strength.required
-					);
-					solver.addConstraint(parent.children.constraint);
-				}
+
+				this.update_parent(parent);
 
 				const u = data[updated];
 				for (const constraint of u.parent.constraints) {
@@ -450,7 +405,7 @@ export class G extends React.Component<MyProps, {}> {
 			console.log(node.position());
 			solver.suggestValue(data[node.id].left, node.position().x);
 			solver.suggestValue(data[node.id].top, node.position().y);
-			changed.add(node.id);
+			changed = new Set([...changed, ...this.propogade_update(node.id)]);
 		} else if (type == "resize") {
 			solver.suggestValue(data[node.id].width, node.size().width);
 			solver.suggestValue(data[node.id].height, node.size().height);
