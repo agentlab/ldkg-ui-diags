2a394da7cb798050f897ba19829a2d158957a28e
Mvwivs <mvwivs@gmail.com>
2021-03-08 17:42:11 +0300

    add nesting, fix performance

--- src/diagram/nested_2.tsx
+++ src/diagram/nested_2.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Graph } from "@antv/x6";
+import { Graph, Node, Cell } from "@antv/x6";
 
 import { ReactShape } from "@antv/x6-react-shape";
 import "@antv/x6-react-shape";
@@ -9,8 +9,8 @@ import { Compartment } from "./Compartment";
 import { NodeField } from "./NodeField";
 
 import { useLocalObservable, observer } from "mobx-react-lite";
-import { toJS } from 'mobx';
 import * as kiwi from "kiwi.js";
+import { v4 as uuidv4 } from 'uuid';
 
 const graphWidth = 800;
 const graphHeight = 600;
@@ -58,7 +58,6 @@ export const Canvas = ({ children }) => {
 		},
 		addEdge(dest_id: string, src_id: string, label: string) {
 			graphStore.deferredEdges[dest_id] = [src_id, label];
-			console.log(toJS(graphStore.deferredEdges));
 		}
 	}));
 
@@ -138,17 +137,33 @@ export const Canvas = ({ children }) => {
 	);
 }
 
-export const NodeBox = observer(({ node, children, edges = [] }: any) => {
+export const NodeBox = observer(({ node, children, parent_id = null, edges = [] }: any) => {
 	const graphStore = React.useContext(graphContext);
+	console.log(`${parent_id} -> ${node.id}`);
+	const [rendered, setRendered] = React.useState<boolean>(false);
 
 	React.useEffect(() => {
+		console.log("1");
 		if (!graphStore.graph) {
 			return;
 		}
-		graphStore.graph.addNode(node);
+		if (parent_id === -1) { // parent not available
+			return;
+		}
+		if (parent_id === null) { // no parent, render to canvas
+			const res = graphStore.graph.addNode(node);
+			console.log("RES:", node);
+		}
+		else {
+			console.log('parent:', parent_id);
+			const parent: Cell = (graphStore.graph as Graph).getCell(parent_id);
+			const child = (graphStore.graph as Graph).addNode(node);
+			parent.addChild(child);
+			console.log(parent.getChildren());
+		}
+		setRendered(true);
 
 		if (graphStore.hasEdge(node.id)) {
-			console.log("HAS!")
 			const [src_id, label] = graphStore.getEdge(node.id);
 			(graphStore.graph as Graph).addEdge({
 				source: src_id,
@@ -158,9 +173,10 @@ export const NodeBox = observer(({ node, children, edges = [] }: any) => {
 			graphStore.deleteEdge(node.id)
 		}
 
-	}, [node, graphStore.graph]);
+	}, [node, parent_id, graphStore.graph]);
 
 	React.useEffect(() => {
+		console.log("2");
 		if (!graphStore.graph) {
 			return;
 		}
@@ -178,13 +194,17 @@ export const NodeBox = observer(({ node, children, edges = [] }: any) => {
 		}
 	}, [edges, node.id, graphStore.graph]);
 
+	const childrenWithProps = React.Children.map(children, child =>
+		React.cloneElement(child, { parent_id: rendered ? node.id : -1 })
+	);
+
 	return (
-		<>{children}</>
+		<>{childrenWithProps}</>
 	);
 });
 
 export const VericalBox = observer((props: any) => {
-	const { data } = props;
+	const { data, parent_id } = props;
 
 	const node = {
 		id: data["@id"],
@@ -192,18 +212,20 @@ export const VericalBox = observer((props: any) => {
 		zIndex: 0,
 		position: randPos(),
 		shape: "group",
-		component: <NodeShape text={data["@id"]} />,
+		component(_) {
+			return (<NodeShape text={data["@id"]} />);
+		},
 	}
+	console.log("high:", node.component);
 
 	const generalFields = Object.entries(data)
 		.filter(([key, val]) => (key !== 'property' && key !== '@id'));
-	let propertyFields = [];
-	if (data['property']) {
-		propertyFields = data['property'].map((prop) => ['sh:property', prop['@id']]);
-	}
+	const propertyFields = data['property']
+		? data['property'].map((prop) => ['sh:property', prop['@id']])
+		: [];
 
 	return (
-		<NodeBox node={node} edges={propertyFields.length > 0 ? propertyFields : []}>
+		<NodeBox node={node} edges={propertyFields} parent_id={parent_id}>
 			{(generalFields.length > 0)
 				? <WrapBox header="General" data={generalFields} />
 				: <></>}
@@ -215,34 +237,40 @@ export const VericalBox = observer((props: any) => {
 });
 
 export const WrapBox = observer((props: any) => {
-	const { header, data } = props;
+	const { parent_id, header, data } = props;
 
 	const node = {
+		id: uuidv4(),
 		size: { width: 200, height: 30 },
 		zIndex: 1,
 		shape: "compartment",
-		component: <Compartment text={header} />,
+		component(_) {
+			return <Compartment text={header} />;
+		},
 	}
 
 	return (
-		<NodeBox node={node}>
+		<NodeBox node={node} parent_id={parent_id}>
 			{data.map(([name, val], idx) => <FieldBox key={idx} text={`${name}:	${val}`} />)}
 		</NodeBox>
 	);
 });
 
 export const FieldBox = observer((props: any) => {
-	const { text } = props;
+	const { parent_id, text } = props;
 
 	const node = {
+		id: uuidv4(),
 		size: { width: 200, height: 50 },
 		zIndex: 2,
 		shape: "field",
-		component: <NodeField text={text} />,
+		component(_) {
+			return <NodeField text={text} />
+		},
 	}
 
 	return (
-		<NodeBox node={node} />
+		<NodeBox node={node} parent_id={parent_id}/>
 	);
 });
 
