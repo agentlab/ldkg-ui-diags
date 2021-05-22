4c6b25d7abcf3fa793b4c8418bca743db43c6740
Mvwivs <37626840+Mvwivs@users.noreply.github.com>
2021-05-22 10:35:51 +0300

    add basic cypress test

---
+++ cypress/integration/class.spec.js
@@ -0,0 +1,5 @@
+describe('Class diagram', () => {
+  it('loads without errors', () => {
+    cy.visit('http://localhost:3000/');
+  });
+});
--- cypress/integration/visit.spec.js
+++
@@ -1,5 +0,0 @@
-describe('My First Test2', () => {
-  it('Visits the Kitchen Sink', () => {
-    cy.visit('http://localhost:3000/');
-  });
-});
