1bf385ff4f33afbfcd8bb327cc276e40bb6caa5e
Mvwivs <37626840+Mvwivs@users.noreply.github.com>
2021-05-23 19:16:19 +0300

    add embed test

--- cypress/integration/class.spec.js
+++ cypress/integration/class.spec.js
@@ -2,4 +2,23 @@ describe('Class diagram', () => {
   it('loads without errors', () => {
     cy.visit('/');
   });
+
+  // TODO: add data seeding or use storybook
+  it('embeds node', () => {
+    cy.visit('/');
+
+    const from = 'Имя пользователя: xsd:dateTime';
+    const to = 'Требование';
+
+    cy.contains(from).trigger('mousedown', { button: 0 });
+    // click simulates move from previos pos to clicking position
+    cy.contains(to).click().trigger('mouseup');
+
+    cy.contains(from)
+      .invoke('width')
+      .then((width) => cy.contains(to).invoke('width').should('be.gte', width));
+    cy.contains(from)
+      .invoke('position')
+      .then((pos) => cy.contains(to).invoke('position').its('top').should('be.lt', pos.top));
+  });
 });
