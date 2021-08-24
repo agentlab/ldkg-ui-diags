4a85f2a6e1fa5ca0842abcf334865c8d9b7f4b86
Artyom <artkrygin@yandex.ru>
2021-08-24 16:32:12 +0300

    [ADD] graphRenderer

---
+++ src/components/graphRenderer.tsx
@@ -0,0 +1,79 @@
+import React, { useContext } from 'react';
+import { rankWith, uiTypeIs, RankedTester, RenderProps, MstContext } from '@agentlab/ldkg-ui-react';
+import { getSnapshot } from 'mobx-state-tree';
+import { observer } from 'mobx-react-lite';
+import { Graph } from './diagram/Graph';
+
+export const graphRendererTester: RankedTester = rankWith(2, uiTypeIs('aldkg:DiagramEditorVKElement'));
+export const testTester: RankedTester = rankWith(2, uiTypeIs('test'));
+const Test = (props) => {
+  console.log('TEST');
+};
+
+export const GraphRenderer: React.FC<RenderProps> = observer(
+  ({ id, viewKind, viewKindElement = {}, viewDescr = {}, viewDescrElement, schema, enabled, form }) => {
+    const { store } = useContext(MstContext);
+    const options = viewKindElement.options || {};
+    const regStencils = (stencils, arr) => {
+      arr.forEach((e) => {
+        if (e.elements) {
+          regStencils(stencils, e.elements);
+        }
+        if (e['@type'] === 'aldkg:DiagramNodeVKElement') {
+          stencils[e['@id']] = e;
+        }
+      });
+    };
+    const stencilPanel: any = {};
+    const viewKindStencils = ((viewKindElement as any)?.elements || []).reduce((acc, e) => {
+      if (e.elements) {
+        regStencils(acc, e.elements);
+      }
+      acc[e['@id']] = e;
+      stencilPanel[e['@id']] = e;
+      return acc;
+    }, {});
+
+    const dataSource = ((viewKindElement as any)?.elements || []).reduce((acc, e) => {
+      if (e.resultsScope) {
+        const dataUri = (viewDescr as any).collsConstrs.filter((el) => el['@parent'] === e.resultsScope);
+        if (dataUri.length > 0) {
+          const graphData = store.getColl(dataUri[0]);
+          acc[e['@id']] = graphData?.data ? getSnapshot(graphData?.data) : [];
+        } else {
+          console.log('No data for element', e);
+        }
+      }
+      return acc;
+    }, {});
+
+    const scope = viewKindElement.resultsScope;
+    const withConnections = options.connections;
+    const onChange = (data: any) => {
+      store.setSelectedData(scope, data);
+      withConnections &&
+        options.connections.forEach((e: any) => {
+          const condition: any = {};
+          condition[e.by] = data[0];
+          store.editCondition(e.to, condition, scope, e.by, data);
+        });
+    };
+
+    return (
+      <Graph
+        view={viewDescrElement}
+        viewDescrObs={viewDescrElement}
+        viewKindStencils={viewKindStencils}
+        stencilPanel={stencilPanel}
+        viewKind={viewKind?.elements[0]}
+        dataSource={dataSource}
+        onSelect={onChange}
+      />
+    );
+  },
+);
+
+export const graphRenderer = [
+  { tester: graphRendererTester, renderer: GraphRenderer },
+  { tester: testTester, renderer: Test },
+];
